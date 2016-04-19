d9447db90293f8260cbfa10c4575608e392af4fb
Pgz <Pgz@Pgz-PC>
2016-04-19 11:34:17 -0300

    códigoSQL (connection)

--- src/java/connection/codigoSql.sql
+++ src/java/connection/codigoSql.sql
@@ -2,17 +2,11 @@ drop database if exists dbfutebolmanager;
 create database dbfutebolmanager;
 
 use dbfutebolmanager;
-
-create table useradmin(
-	iduseradmin int not null auto_increment primary key,
-    `name` varchar(100),
-    login varchar(50),
-    pass varchar(50)
-    );
     
 create table jogador(
-	idJogador int not null auto_increment primary key,
-    iduseradmin int not null,
+    idJogador int not null auto_increment primary key,
+    login varchar(50),
+    pass varchar(50),
     nomeJogador varchar(255),
     dtNascimento date,
     endereco varchar(255),
@@ -23,46 +17,18 @@ create table jogador(
 	
 );
 
-create table Team(
-	idTeam int not null auto_increment primary key,
-    iduseradmin int not null,
-    teamName varchar(255),
-    endereco varchar(255),
-    cnpj int ,
-    foundationDate date,
-    president varchar(255),
-    email varchar(255) ,
-		
-	CONSTRAINT FK_TEAM_USERADMIN FOREIGN KEY (IDUSERADMIN) REFERENCES USERADMIN(IDUSERADMIN)
-);
-
 create table image(
-	idimage int not null auto_increment primary key,
-    iduseradmin int not null,
-    idjogador int not null,
-    idteam int not null,
+    idimage int not null auto_increment primary key,
     nome varchar(100),
     descricao varchar(255),
     tamanho varchar(255),
-    caminho varchar(255),
-	
-	CONSTRAINT FK_IMAGE_USERADMIN FOREIGN KEY (IDUSERADMIN) REFERENCES USERADMIN(IDUSERADMIN),
-	
-	CONSTRAINT FK_IMAGE_JOGADOR FOREIGN KEY (IDJOGADOR) REFERENCES JOGADOR(IDJOGADOR),
-		
-	CONSTRAINT FK_IMAGE_TEAM FOREIGN KEY (IDTEAM) REFERENCES TEAM(IDTEAM)
+    caminho varchar(255)
     
 );
 
 create table telefone(
-	idTelefone int not null auto_increment primary key,
-    iduseradmin int not null,
-    idjogador int not null,
-    numeroTelefone int,
-	
-	CONSTRAINT FK_TELEFONE_USERADMIN FOREIGN KEY (IDUSERADMIN) REFERENCES USERADMIN(IDUSERADMIN),
-	
-	CONSTRAINT FK_TELEFONE_JOGADOR FOREIGN KEY (IDJOGADOR) REFERENCES JOGADOR(IDJOGADOR)
+    idTelefone int not null auto_increment primary key,
+    numeroTelefone int
 );
 
 
